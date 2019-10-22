c3a24d83edd28631b7fb2e7563b40edce1bea0da
bianca <bianca@buscaprev.com.br>
2019-10-21 21:23:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Estudo do ganho social em redes sociais
+
+Projeto desenvolvido em python para simular o comportamento de individuos e observar o ganho social aplicando 
+conhecimentos de Teoria de Jogos.
+
+
+## Instalação do projetos
+2. `$ virtualenv --system-site-packages -p python3 env`
+3. `$ source env/bin/activate`
+4. `$ pip install -r requirements.txt`
---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+V1 da Simulacao:
+1- ter a definição os usuários que possuem perfis(com atributos de acordo com a equação de utilidade)
+2- gerar uma populacao de usuários aleatoriamente (com tamanho y)
+3- fazer a rodada de 1 user x todos os outros até que esse user tenha jogado com tds os outros
+4- imprime resultados => valor da função de utilidade geral (somatório de todas as utlidades obtidas nas interações)
+"""
