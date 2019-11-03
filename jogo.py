3ae28c8f0e1b318c987bb35aa7ff38935a18efc0
bianca <bianca@buscaprev.com.br>
2019-11-03 17:13:34 -0300

    mapeando o que precisa ser feito no jogo

--- jogo.py
+++ jogo.py
@@ -7,9 +7,17 @@ def jogadas(jogadores: list):
         o resultado da jogada individual é a utilidade daquela interação, esse resultado deve ser armazenado e
         incrementado a cada iteração do jogador com outro membro da população.
     """
+    for jogador in jogadores:
+        print('for jogador')
+        # fazer a jogada individual desse jogador com cada um um dos outros e armazenar o resultado de cada uma delas
+        # fazer a media das utilidades de todas as jogads desse jogador
     pass
 
 
 def jogada_individual(jogador: Player):
+    pass
 
+
+def utilidade_da_jogada():
+    # calcular a media das utilidades
     pass
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from populacao import gera_populacao
 
 
 def main(args):
-    # TODO implementar um gera populacao aleatoria utilizando o args.n_pop para definir o tamanho da pop
+    # TODO implementar um gera populacao aleatoria utilizando o args.npop para definir o tamanho da pop
     populacao = gera_populacao(args.txt)
     jogadas(populacao)
 
@@ -15,7 +15,7 @@ def arg_parse():
     """ analisa e separa os argumentos passados ao iniciar a execucao """
     parser = argparse.ArgumentParser(description='new')
     parser.add_argument("--txt", help="Endereço do TXT a ser carregado com a população", default="")
-    parser.add_argument("--n_pop", help="Número da população a ser criada", type=int, default=0)
+    parser.add_argument("--npop", help="Número da população a ser criada", type=int, default=0)
 
     return parser.parse_args()
 
