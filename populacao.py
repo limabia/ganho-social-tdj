7c2040f21c21175810b52b87e688058f93bc7483
Gabriel Estavaringo <gabriel.estavaringo@gmail.com>
2019-11-11 20:32:27 -0300

    read files

--- populacao.py
+++ populacao.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+# generate random integer values
+from random import seed
+from random import randint
 
 
 class Jogador:
@@ -22,26 +25,85 @@ class Jogador:
         self.minimoConsumo = float(minimo_consumo)
 
 
-def gera_populacao(txt):
-    """ func geradora de população atraves de um txt"""
-    massa_populacao = open(txt, "r")
 
-    populacao = []
+def gera_jogador(atributos):
+    jogador = Jogador(
+        perfil=atributos[0],
+        conteudo_interesse=atributos[1],
+        conteudo_publicado=atributos[2],
+        qualidade_publicacao=atributos[3],
+        frequencia_publicacao=atributos[4],
+        minimo_consumo=atributos[5]
+    )
+    return jogador
 
-    if massa_populacao.mode == 'r':
-        contents = massa_populacao.readlines()
+
+def gera_jogador_aleatorio(lista_atributos):
+    # seed random number generator
+    seed(1)
+    randomIndex = randint(0, len(lista_atributos))
+    atributos = lista_atributos[randomIndex]
+    return gera_jogador(atributos)
+
+
+def gera_getalhes(pathFile):
+    fileOpen = open(pathFile, "r")
+    lista = []
+    if fileOpen.mode == 'r':
+        contents = fileOpen.readlines()
         while contents:
             linha = contents.pop().replace("\n", "")
             atributos = linha.split(",")
-            jogador = Jogador(
-                perfil=atributos[0],
-                conteudo_interesse=atributos[1],
-                conteudo_publicado=atributos[2],
-                qualidade_publicacao=atributos[3],
-                frequencia_publicacao=atributos[4],
-                minimo_consumo=atributos[5]
-            )
-            populacao.append(jogador)
-    massa_populacao.close()
+            lista.append(atributos)
+    fileOpen.close()
+    return lista
+
+
+def gera_populacao(tipo_perfis, detalhes_pop):
+    populacao = []
+
+    lista_detalhes_pop = gera_getalhes(detalhes_pop)
+    lista_perfis = gera_getalhes(tipo_perfis)
+
+    for perfil in lista_perfis:
+        if(perfil[0] == 'perfil1'):
+            perfil1 = detalhe_pop[1]
+        if(perfil[0] == 'perfil2'):
+            perfil2 = detalhe_pop[1]
+        if(perfil[0] == 'perfil3'):
+            perfil3 = detalhe_pop[1]
+        if(perfil[0] == 'perfil4'):
+            perfil4 = detalhe_pop[1]
+
+    for detalhe_pop in lista_detalhes_pop:
+        if(detalhe_pop[0] == 'pop'):
+            total_pop = detalhe_pop[1]
+
+        if(detalhe_pop[0] == 'perfil1'):
+            qtd_perfil1 = detalhe_pop[1]
+            for i in range(qtd_perfil1):
+                populacao.append(gera_jogador(perfil1))
+            total_pop -= qtd_perfil1
+
+        if(detalhe_pop[0] == 'perfil2'):
+            qtd_perfil2 = detalhe_pop[1]
+            for i in range(qtd_perfil2):
+                populacao.append(gera_jogador(perfil2))
+            total_pop -= qtd_perfil2
+
+        if(detalhe_pop[0] == 'perfil3'):
+            qtd_perfil3 = detalhe_pop[1]
+            for i in range(qtd_perfil3):
+                populacao.append(gera_jogador(perfil3))
+            total_pop -= qtd_perfil3
+
+        if(detalhe_pop[0] == 'perfil4'):
+            qtd_perfil4 = detalhe_pop[1]
+            for i in range(qtd_perfil4):
+                populacao.append(gera_jogador(perfil4))
+            total_pop -= qtd_perfil4
+
+    for i in range(total_pop):
+        populacao.append(gera_jogador_aleatorio(lista_perfis))
 
     return populacao
